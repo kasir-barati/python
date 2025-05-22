daefd6b06364ae1f5a1fc6cd60944b6c8458ff90
Mohammad Jawad Barati <kasir.barati@gmail.com>
2025-05-22 18:16:22 +0200

    chore: wip -- rabbitmq

---
+++ docs/rabbitmq/src/constants.py
@@ -0,0 +1,4 @@
+"""Constant variables"""
+
+REGISTERED_USER_QUEUE = "user.registered"
+REGISTERED_USER_QUEUE_ROUTING_KEY = "user.registered"
---
+++ docs/rabbitmq/src/consumer.py
@@ -0,0 +1,19 @@
+import pika
+import constants
+
+
+# Create connection string
+connection_params = pika.ConnectionParameters('localhost', 5672)
+# Create connection
+connection = pika.BlockingConnection(connection_params)
+# Create a channel
+channel = connection.channel()
+# Declare queue
+channel.queue_declare(constants.REGISTERED_USER_QUEUE)
+
+
+# Consume messages
+channel.basic_consume(constants.REGISTERED_USER_QUEUE,
+                      lambda ch, method, properties, message: print(message),
+                      True)
+channel.start_consuming()
