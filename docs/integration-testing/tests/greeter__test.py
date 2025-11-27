eabd24a721546cf1794ac992058737a642592297
Kasir Barati <kasir.barati@gmail.com>
2025-11-27 09:49:43 +0100

    chore: cleanup

--- docs/integration-testing/tests/greeter__test.py
+++ docs/integration-testing/tests/greeter__test.py
@@ -5,7 +5,6 @@ from stubs.helloworld_pb2_grpc import GreeterStub
 
 
 def test_grpc_client(grpc_server):
-    print(f"🧪 TEST 1: Using server at {grpc_server}")
     channel = grpc.insecure_channel(grpc_server)
     stub = GreeterStub(channel)
 
@@ -15,7 +14,6 @@ def test_grpc_client(grpc_server):
 
 
 def test_grpc_client_second_call(grpc_server):
-    print(f"🧪 TEST 2: Using server at {grpc_server}")
     channel = grpc.insecure_channel(grpc_server)
     stub = GreeterStub(channel)
 
